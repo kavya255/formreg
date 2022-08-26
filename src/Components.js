07e6bb22e3fd52a3a78b88a6b9bce97413018068
kavya <kavya@kavyas-MacBook-Air.local>
2022-08-26 10:29:37 +0530

    final

--- src/Components.js
+++ src/Components.js
@@ -5,93 +5,20 @@ export const InputComponent = (props) => {
     <div
       style={{
         display: "flex",
-        flexDirection: props.type === "text" ? "column" : "row",
+        flexDirection:
+          props.type === "text" || props.type === "email" ? "column" : "row",
       }}
     >
       {props.type === "radio" && <input {...props}></input>}
       <TextComponent
         label={props.label}
-        CustomTag={props.type === "text" ? "div" : "span"}
+        CustomTag={
+          props.type === "text" || props.type === "email" ? "div" : "span"
+        }
       />
-      {props.type === "text" && <input {...props}></input>}
-    </div>
-  );
-};
-export const Button = (props) => {
-  return (
-    <div>
-      <button
-        style={{
-          backgroundColor: "#8D19FFB2",
-          marginTop: "40px",
-          marginLeft: "1700px",
-          padding: "10px",
-          borderRadius: "5px",
-        }}
-        onClick={() =>
-          props.clickEvent(props.updateStateMethod, props.stateVariable)
-        }
-      >
-        {props.label}
-      </button>
-    </div>
-  );
-};
-
-export const DatePick = (props) => {
-  const { onChange, value } = props;
-  return (
-    <div>
-      <label>
-        {
-          <TextComponent
-            label="DOB-(DD/MM/YY)"
-            isMandatory="true"
-            styleProps={{
-              fontSize: "16px",
-              textAlign: "left",
-              fontWeight: 700,
-            }}
-          />
-        }
-      </label>
-      <input type="date" name="DOB" onChange={onChange} value={value} />
-    </div>
-  );
-};
-
-export const Dropdown = (props) => {
-  const { heading,value } = props;
-  // const a = ["select", "undergraduate", "diploma",'post'];
-  return (
-    <div>
-      <label>
-        <div> {heading}</div>{" "}
-      </label>
-
-      <select>
-        {value.map((item) => {
-          return <option value=""> {item}</option>;
-        })}
-
-        {/* <option value=""> undergraduate</option>
-        <option value=""> diploma</option>
-        <option value=""> postgraduate</option> */}
-      </select>
-    </div>
-  );
-};
-
-export const Dropdown2 = () => {
-  return (
-    <div>
-      <br></br>
-      <TextComponent label="Backlogs" isMandatory={"true"} />
-
-      <select>
-        <option value=""> No</option>
-        <option value=""> Yes</option>
-      </select>
+      {(props.type === "text" || props.type === "email") && (
+        <input {...props}></input>
+      )}
     </div>
   );
 };
@@ -112,18 +39,89 @@ export const TextComponent = ({
 
 export const Box = (props) => (
   <>
-    <TextComponent label={props.header} />
+    <TextComponent
+      label={props.header}
+      styleProps={{
+        fontSize: "24px",
+        textAlign: "left",
+        fontWeight: 700,
+        margin: "10px",
+      }}
+    />
     <div
       style={{
-        border: "1px solid",
-        borderTopLeftRadius: "5px",
-        borderBottomLeftRadius: "5px",
-        borderTopRightRadius: "5px",
-        borderTopLeftRadius: "5x",
+        border: "1px solid #E5E5E5",
+        borderRadius: "10px",
         padding: "10px",
+        fontFamily: "Hind",
+        margin: "20px",
       }}
     >
       {props.children}
     </div>
   </>
 );
+
+export const DatePick = () => {
+  return (
+    <div>
+      <label>
+        {
+          <TextComponent
+            label="DOB-(DD/MM/YY)"
+            isMandatory="true"
+            styleProps={{
+              fontSize: "16px",
+              textAlign: "left",
+              fontWeight: 700,
+              paddingTop: "10px",
+            }}
+          />
+        }
+      </label>
+      <input type="date" name="DOB" />
+    </div>
+  );
+};
+
+export const Dropdown = (props) => {
+  return (
+    <div>
+      <select>
+        {props.options?.map((m) => (
+          <option value={m}>{m}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export const Attatchments = () => {
+  return (
+    <>
+      <div>
+        <TextComponent
+          styleProps={{
+            fontSize: "18px",
+            textAlign: "left",
+            fontWeight: 700,
+            margin: 10,
+          }}
+          label="Add Resume/CV"
+          CustomTag="span"
+        >
+          <TextComponent
+            styleProps={{
+              fontSize: "18px",
+              textAlign: "left",
+              fontWeight: 400,
+            }}
+            CustomTag="span"
+            label="(Accept format: doc/.docx/.pdf;Size: 1MB)"
+          />
+        </TextComponent>
+      </div>
+      <button>Choose File</button>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { TextComponent, InputComponent, Box , DatePick,Dropdown, Dropdown2,Button} from "./Components";
+//import ReactDOM from "react-dom";
+import {
+  TextComponent,
+  InputComponent,
+  Box,
+  DatePick,
+  Dropdown,
+  Attatchments,
+} from "./components";
+import GlobalStyle from "./globalStyles";
+import {postReq} from "../pages/index";
+
+///import styled from "styled-components";
 
 const Header = () => {
   return (
@@ -9,352 +20,375 @@ const Header = () => {
         backgroundColor: "#8D19FFB2",
         justifyContent: "center",
         alignItems: "center",
+        color: "white",
+        fontFamily: "Helvetica",
+        fontWeight: "700",
       }}
     >
       <TextComponent
         label="INCRESCO TECHNOLOGIES"
-        styleProps={{ fontSize: "48px", textAlign: "center",fontVariant:"Helvetica",color:"#FFFFFF" ,fontWeight:"700"}}
+        styleProps={{
+          fontSize: "48px",
+          textAlign: "center",
+          paddingTop: "15px",
+        }}
       />
       <br></br>
       <TextComponent
         label="CAMPUS HIRING 2023"
-        styleProps={{ fontSize: "36px", textAlign: "center", fontVariant:"Helvetica",color:"#FFFFFF",fontWeight:"700"}}
+        styleProps={{
+          fontSize: "36px",
+          textAlign: "center",
+          paddingBottom: "15px",
+        }}
       />
     </div>
   );
 };
 
-
-
-const Attatchments = () => {
-  return (
-    <>
-      <div>
-      
-        <h3>ATTATCHMENTS</h3>
-<Box>
-        <TextComponent
-          styleProps={{ fontSize: "18px", textAlign: "left", fontWeight:"700 "}}
-          label="Add Resume/CV"
-          isMandatory={"true"}
-          CustomTag="span"
-        >
-        
-          <TextComponent
-            styleProps={{
-              fontSize: "18px",
-              textAlign: "left",
-              fontWeight: "400",
-            }}
-            CustomTag="span"
-            label="Accept format: doc/.docx/.pdf;Size: 1MB"
-          />
-          
-        </TextComponent>
-       
-      <div>
-      <button> Choose File</button></div>
-      </Box>
-      </div>
-    </>
-    
-  );
-};
-
-const App = () => {
+export const App = ({data}) => {
   const [firstName, setFirstName] = useState("");
-  const [middleName,setMiddleName] =useState("");
   const [lastName, setLastName] = useState("");
-  const [emailid, setEmailId] = useState("");
-  const [contactnumber, setContactNumber] = useState("");
-  const [dob, setDob] = useState("");
+  const [middleName, setMiddleName] = useState("");
   const [gender, setGender] = useState("");
-  const [tenth, setTenthPercentage] = useState("");
-  const [twelfth, setTwelfthPercentage] = useState("");
-  const [degree, setDegree] = useState("");
-  const [college, setCollege] = useState("");
-  const [regno, setRegNo] = useState("");
-  const [branch, setBranch] = useState("");
-  
-  console.log({dob})
+  const [mail, setEmail] = useState("");
+  const [number, setNumber] = useState("");
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
-      <form>
-      <TextComponent
-
-      label="Required"
-      isMandatory={"true"}
-       />
-        <h1>Registration Form</h1>
-        <TextComponent />
-        <b>  job details</b>
-        <Box>
-        job profile
-          <InputComponent
-          type="text"
-          //label=<b>job profile</b>
-          isMandatory={"true"}
-          placeholder="Software Developer"
-          disabled/>
-         </Box>
-         <form >
-
-         <br></br>
-         <b>Personal details</b>
-        <Box>
-<TextComponent 
-           label="First Name"
-           isMandatory={"true"}/>
+      <TextComponent label="*Required" styleProps={{ margin: "10px" }} />
+      <div>
+        <TextComponent
+          label="Registration Form"
+          styleProps={{ fontWeight: 700, fontSize: "48px", margin: "10px" }}
+        />
+        <Box header="Job Details">
           <InputComponent
             type="text"
-           // label="First Name"
+            placeholder="Software Developer"
+            disabled
+            style={{ height: "2em" }}
+            label={
+              <TextComponent
+                label="Job Profile:"
+                isMandatory="true"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                }}
+              />
+            }
+          />
+        </Box>
+        <Box header="Personal Details">
+          <InputComponent
+            type="text"
+            style={{ height: "2em" }}
+            label={
+              <TextComponent
+                label="First Name:"
+                isMandatory="true"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+              />
+            }
             placeholder="Enter First name"
             value={firstName}
             onChange={(a) => {
-              console.log(a.target.value);
               setFirstName(a.target.value);
             }}
           />
-        <br></br>
-        
-          <InputComponent
-          type="text"
-        
-          label="Middle Name"
-          placeholder="Enter Middle name"
-          value={middleName}
-          onChange={(a) =>{ 
-            console.log(a.target.value);
-            setMiddleName(a.target.value);
-          }}
-          />
-          <br></br>
-          
-           <TextComponent 
-           label="Last Name"
-           isMandatory={"true"}/>
           <InputComponent
             type="text"
+            style={{ height: "2em" }}
+            label={
+              <TextComponent
+                label="Middle Name:"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+              />
+            }
+            placeholder="Enter middle name"
+            value={middleName}
+            onChange={(a) => {
+              setMiddleName(a.target.value);
+            }}
+          />
+          <InputComponent
+            type="text"
+            style={{ height: "2em" }}
+            label={
+              <TextComponent
+                label="Last Name:"
+                isMandatory="true"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+              />
+            }
             placeholder="Enter last name"
             value={lastName}
             onChange={(a) => {
-              console.log(a.target.value);
               setLastName(a.target.value);
-              
             }}
           />
-          <br>
-
-          </br>
-           <TextComponent 
-           label="Email Id"
-           isMandatory={"true"}/>
+          <InputComponent
+            type="email"
+            style={{ height: "2em" }}
+            label={
+              <TextComponent
+                label="Email:"
+                isMandatory="true"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+              />
+            }
+            placeholder="@gmail.com"
+            value={mail}
+            onChange={(a) => {
+              setEmail(a.target.value);
+            }}
+          />
           <InputComponent
             type="text"
-            placeholder="kavya@gmail.com"
-            value={emailid}
+            style={{ height: "2em" }}
+            pattern="[0-9]+"
+            value={number}
+            label={
+              <TextComponent
+                label="Contact Number:"
+                isMandatory="true"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+              />
+            }
+            placeholder="123456789"
+            maxLength="10"
+            minLength="10"
             onChange={(a) => {
-              console.log(a.target.value);
-              setEmailId(a.target.value);
-              
+              setNumber(a.target.value);
             }}
           />
-          <br>
-
-          </br>
-           <TextComponent 
-           label="Contact Number"
-           isMandatory={"true"}/>
-          <InputComponent
-            type="text"
-            //label="Contact Number"
-            isMandatory={"true"}
-            placeholder="999999999h"
-            value={contactnumber}
-            onChange={(a) => {
-              console.log(a.target.value);
-              setContactNumber(a.target.value);
-              
-            }}
-          />
-          <br></br>
-          <DatePick onChange={(data)=>setDob(data.target.value)} value={dob}/>
-        <br>
-          
-        </br>
+          <DatePick />
           <TextComponent
-        label="Gender"
-        styleProps={{ fontSize: "18px", textAlign: "left" }}
-        isMandatory="true"
-      />
+            label="Gender"
+            isMandatory="true"
+            styleProps={{
+              fontSize: "16px",
+              textAlign: "left",
+              fontWeight: 700,
+              paddingTop: "10px",
+            }}
+          />
           <InputComponent
-              type="radio"
-              value="Male"
-              name="gender"
-              label="Male"
-
-              onChange={(a) => {
-                console.log(a.target.value);
-                setGender(a.target.value);
-              }}
-             
-            />
-            <InputComponent
-              type="radio"
-              value="Female"
-              name="gender"
-              label="Female"
-
-              onChange={(a) => {
-                console.log(a.target.value);
-                setGender(a.target.value);
-              }}
-             
-            />
-            <InputComponent
-              type="radio"
-              value="Other"
-              name="gender"
-              label="Other"
-
-              onChange={(a) => {
-                console.log(a.target.value);
-                setGender(a.target.value);
-              }}
-            />
-          
+            type="radio"
+            value={gender}
+            name="gender"
+            label="Male"
+            onChange={(a) => {
+              setGender(a.target.value);
+            }}
+          />
+          <InputComponent
+            type="radio"
+            value={gender}
+            name="gender"
+            label="Female"
+            onChange={(a) => {
+              setGender(a.target.value);
+            }}
+          />
+          <InputComponent
+            type="radio"
+            value={gender}
+            name="gender"
+            label="Other"
+            onChange={(a) => {
+              setGender(a.target.value);
+            }}
+          />
         </Box>
-        </form>
-        <h3>Educational details</h3> 
-         <Box>
-         
-        
-         
-         <h4> <header> Lowest education profile</header> </h4>
-          <TextComponent 
-           label="10th percentage %"
-           isMandatory={"true"}/>
-           <InputComponent
-             
-            type="text"
-            //label="10th percentage %"
-          
-            placeholder="56%"
-            value={tenth}
-            onChange={(a) => {
-              
-              console.log(a.target.value);
-              setTenthPercentage(a.target.value); 
-              
-              
+        <Box header="Educational Details">
+          <TextComponent
+            label="Lowest Educational profile"
+            styleProps={{
+              fontSize: "16px",
+              textAlign: "left",
+              fontWeight: 700,
+              paddingTop: "10px",
             }}
           />
-          <br>
-            
-          </br>
-          <TextComponent 
-           label="12th percentage %"
-           isMandatory={"true"}/>
-           <InputComponent
+          <InputComponent
+            style={{ height: "2em" }}
             type="text"
-            //label="12th percentage %"
-            isMandatory={"true"}
-            placeholder="70%"
-            value={twelfth}
-            onChange={(a) => {
-              console.log(a.target.value);
-              setTwelfthPercentage(a.target.value);
-              
-            }}
+            label={
+              <TextComponent
+                label="10th percentage %"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              />
+            }
           />
-         
-         <br></br>
-        
-          <Dropdown />
-          {/* <TextComponent 
-           label="Degree"
-           isMandatory={"true"}/> */}
-
-
           <InputComponent
             type="text"
-            label={<TextComponent label= "degree" isMandatory={"true"}/>}
-            isMandatory={"true"}
-            placeholder="B.E"
-            value={degree}
-            onChange={(a) => {
-              console.log(a.target.value);
-              setDegree(a.target.value);
-              
+            style={{ height: "2em" }}
+            label={
+              <TextComponent
+                label="12th percentage %"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              />
+            }
+          />
+          <TextComponent
+            label="Highest Educational profile"
+            styleProps={{
+              fontSize: "16px",
+              textAlign: "left",
+              fontWeight: 700,
+              paddingTop: "10px",
             }}
-            
-            
-          />  
+          />
+          <TextComponent
+            label="Education Type"
+            isMandatory
+            styleProps={{
+              fontSize: "16px",
+              textAlign: "left",
+              fontWeight: 700,
+              paddingTop: "10px",
+            }}
+          />
           
-          <br></br>
-          <TextComponent 
-           label="College"
-           isMandatory={"true"}/>
-        
-        <InputComponent
+          <Dropdown placeholder="-select-" options={[...data[2].map((a) => a.displayText)]} />
+            
+          
+          
+              <TextComponent
+                label="Degree"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              />
+              
+          
+              <Dropdown placeholder="-select-" options={[...data[0].map((a) => a.displayText)]} />
+          
+          
+          <InputComponent
             type="text"
-           // label="College"
+            style={{ height: "2em" }}
             placeholder="College"
-            value={college}
-            onChange={(a) => {
-              console.log(a.target.value);
-              setCollege(a.target.value);
-              
-            }}
-          />  
-          <br></br>
-          <TextComponent 
-           label="Reg.No"
-           isMandatory={"true"}/>
-        <InputComponent
-            type="text"
-            //label="Reg.No"
-            placeholder="19EUEE062"
-            width= "1198px"
-            value={regno}
-            onChange={(a) => {
-              console.log(a.target.value);
-              setRegNo(a.target.value);
-              
-            }}
-          />  
-          <br>
-
-          </br>
-
+            label={
+              <TextComponent
+                label="College"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              />
+            }
+          />
           <InputComponent
             type="text"
-            label={<TextComponent label="Branch" isMandatory={"true"}/>}
-            placeholder="Computer Science Engineering "
-            value={branch}
-            onChange={(a) => {
-              console.log(a.target.value);
-              setBranch(a.target.value);
-              
-            }}
-          />   
-          <Dropdown2/>
-          </Box>
-          <Box>
-        
-        <Attatchments />
-        <div style={{alignItems:"right"}}>
-        
-        <Button label="submit"/>
-         
-        
-        </div>
+            style={{ height: "2em" }}
+            placeholder="19EUEE062"
+            label={
+              <TextComponent
+                label="Reg.No"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              />
+            }
+          />
+          
+              <TextComponent
+                label="Branch"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              />
+              <Dropdown options={[...data[1].map((a) => a.displayText)]}/>
+            
+          
+              <TextComponent
+                label="Backlogs"
+                styleProps={{
+                  fontSize: "16px",
+                  textAlign: "left",
+                  fontWeight: 700,
+                  paddingTop: "10px",
+                }}
+                isMandatory
+              /> <Dropdown options={["YES","NO"]}/>
+    
         </Box>
-        
-      </form>
+        <Box header="ATTATCHMENTS">
+          <Attatchments />
+        </Box>
+        <button
+          className="submit"
+          style={{
+            backgroundColor: "#8D19FFB2",
+            padding: "8px",
+            margin: "10px",
+            borderRadius: "10",
+          }}
+          onClick={postReq}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
-ReactDOM.render(<App />, document.getElementById("app"));
+
+//ReactDOM.render(<App />, document.getElementById("app"));
